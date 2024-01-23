56c124913a7ef6f7a973636aab6074a89fefe4ed
cae1814 <cae1814@gmail.com>
2024-01-23 17:34:15 -0600

    CambiosFinalizados

---
+++ EjercicioClase/SuperPoder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase
+{
+    internal class SuperPoder
+    {
+        private string nombre;
+        private string descripcion;
+        private int nivel;
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Descripcion
+        {
+            get
+            {
+                return descripcion;
+            }
+            set
+            {
+                descripcion = value;
+            }
+        }
+
+        public int Nivel
+        {
+            get
+            {
+                return nivel;
+            }
+            set
+            {
+                nivel = value;
+            }
+        }
+    }
+}
